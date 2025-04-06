ca52299c081f9fe2d946fae8d1298c79703363cd
promaxgh <70668977+promaxgh@users.noreply.github.com>
2025-04-06 10:18:04 +0000

    Add files via upload

--- drf_course/settings.py
+++ drf_course/settings.py
@@ -45,7 +45,7 @@ ROOT_URLCONF = 'drf_course.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
