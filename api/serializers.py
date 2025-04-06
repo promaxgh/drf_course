378e9761cbff6aaaaf617489299c369118fb8ce1
promaxgh <70668977+promaxgh@users.noreply.github.com>
2025-04-06 01:07:37 +0000

    Add files via upload

---
+++ api/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+
+from .models import User, Product, Order, OrderItem
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = (
+            'id',
+            'name',
+            'description',
+            'price',
+            'stock'
+        )
+
+    def validate_price(self, value):
+        if value <= 0:
+            raise serializers.ValidationError(
+                'Price must be greater than 0.'
+            )
+        
+        return value
+    
